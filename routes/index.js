af5dfd1c3cebd6bef3dc3e62c6bab2b74dfc9832
CarlosSantanaSP <carlos.santana@bandtec.com.br>
2020-12-13 03:45:42 -0300

    Final

--- routes/index.js
+++ routes/index.js
@@ -83,4 +83,10 @@ function send_message_slack(message) {
 }
 
 
+
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
 module.exports = router;
